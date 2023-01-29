636047ec276146b44cd1180c4d5b64b2a44a2200
KeepKindling <flashywizard127@gmail.com>
2023-01-29 14:22:32 +0000

    fixed typo in ALLOWED_HOST in settings.py. Third Deployment Test

--- food4family/settings.py
+++ food4family/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['food4family.herokyapp.com', 'localhost']
+ALLOWED_HOSTS = ['food4family.herokuapp.com', 'localhost']
 
 
 # Application definition
