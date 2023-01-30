5edda281f2e95489d30ad8e8967816abb51ca83c
KeepKindling <flashywizard127@gmail.com>
2023-01-30 20:09:22 +0000

    added filters for admin Entry panel

--- food_entries/admin.py
+++ food_entries/admin.py
@@ -6,4 +6,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Entry)
 class EntryAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'description']
     summernote_fields = ('description')
--- food_entries/models.py
+++ food_entries/models.py
@@ -7,6 +7,7 @@ STATUS = ((0, "Waiting approval"), (1, "Published"))
 
 class Entry(models.Model):
     title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="entry_recipe")  # noqa
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
@@ -14,7 +15,6 @@ class Entry(models.Model):
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     likes = models.ManyToManyField(User, related_name='entry_likes')
-    slug = models.SlugField(max_length=150, unique=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
