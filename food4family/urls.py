d89f6f4f4c474b005fefb53681337d1bcc87fb79
KeepKindling <flashywizard127@gmail.com>
2023-01-31 10:39:42 +0000

    filled in base.html and corrected typo in urls.py

--- food4family/urls.py
+++ food4family/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("food_entries.urls"), name="food_entries-urls"),
     path('summernote/', include('django_summernote.urls')),
 ]
---
+++ food_entries/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    path("", views.EntryList.as_view(), name="my_recipes"),
+]
