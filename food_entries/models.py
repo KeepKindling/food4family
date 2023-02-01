de9198f7d3f3b2ef7c50bfdea74b4a9c000f244a
KeepKindling <flashywizard127@gmail.com>
2023-02-01 21:23:07 +0000

    created FamilyMember Model

--- food_entries/models.py
+++ food_entries/models.py
@@ -7,16 +7,16 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 class Entry(models.Model):
     title = models.CharField(max_length=150, unique=True)
-    slug = models.SlugField(max_length=150, unique=True)
+    # slug = models.SlugField(max_length=150, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="entry_recipe")  # noqa
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     description = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
-    excerpt = models.TextField(blank=True)
-    likes = models.ManyToManyField(User, related_name='entry_likes', blank=True)  # noqa
-    status = models.IntegerField(choices=STATUS, default=0)
-    approved = models.BooleanField(default=False)
+    # excerpt = models.TextField(blank=True)
+    # likes = models.ManyToManyField(User, related_name='entry_likes', blank=True)  # noqa
+    status = models.IntegerField(choices=STATUS, default=1)
+    # approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["-created_on"]
@@ -24,5 +24,13 @@ class Entry(models.Model):
     def __str__(self):
         return self.title
 
-    def number_of_likes(self):
-        return self.likes.count()
+
+class FamilyMember(models.Model):
+    name = models.CharField(max_length=150)
+    # slug = models.SlugField(max_length=150, unique=True)
+    head_of_family = models.ForeignKey(User, on_delete=models.CASCADE, related_name="family_members")  # noqa
+    likes = models.TextField()
+    dislikes = models.TextField()
+
+    def __str__(self):
+        return self.name
